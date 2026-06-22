6843b61120706ab2377c72dcbaaae2818e14e9bc
xavier <xavier@srv751671.hstgr.cloud>
2026-06-22 17:31:50 +0000

    chore

--- backend/api/entrypoint.sh
+++ backend/api/entrypoint.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 set -e
 
-echo "⏳ Gerando o Prisma client..."
+echo "Gerando o Prisma client..."
 # npm run db:generate
 
-echo "⏳ Sincronizando a base de dados..."
+echo "Sincronizando a base de dados..."
 
 if [ "$NODE_ENV" = "development" ]; then
     echo "--- AMBIENTE DE DESENVOLVIMENTO ---"
-    echo "⏳ Migrando a base de dados..."
+    echo "Migrando a base de dados..."
     # npx prisma migrate reset --force
-    # npx prisma migrate dev --name init
-    echo "🚀 Iniciando servidor em modo desenvolvimento..."
-    npm run start:dev
+    npx prisma db push
+    echo "Iniciando servidor em modo desenvolvimento..."
+    npm run dev
 else
     echo "--- AMBIENTE DE PRODUÇÃO ---"
-    echo "⏳ Deployando migrações..."
+    echo "Deployando migrações..."
     # npx prisma migrate deploy
-    echo "⏳ Buildando o projecto..."
+    echo "Buildando o projecto..."
+    npx prisma db push
     npm run build
-    echo "🚀 Iniciando servidor em modo produção..."
+    echo "Iniciando servidor em modo produção..."
     npm run start:prod
 fi
-
-echo "🚀 Servidor iniciado com sucesso!"
