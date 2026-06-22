a10d159d0604c64816f928afe815d63fb6783628
xmaj2001 <xmaj2001@gmail.com>
2026-06-22 13:32:09 +0100

    chore: ignore typescript build errors in next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // Allow all images for now
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactCompiler: true,
   images: {
     unoptimized: true,
